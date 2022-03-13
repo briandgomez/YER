db2d3cad842b4ecae72b201644a5b613b95d778a
briandgomez <briandgomez98@gmail.com>
2022-03-13 14:50:45 -0400

    created cards for images

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,9 +1,92 @@
 import React from "react";
+import pic from '../../TMDB Color Scheme.png';
 
 const Movies = () => {
     return (
         <div className="movies">
             <h1>Movies</h1>
+            <div style={{ display: "flex", flexWrap: 'wrap', justifyContent: "center" }}>
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Lorem</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+                </div>
+
+
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src={pic} />
+                    <div className="container">
+                        <h4><b>John Doe</b></h4>
+                        <p>Architect & Engineer</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,9 +4,10 @@ function Footer() {
     return (
         <footer>
             <div>
-                <p>Created with:</p>
+                <p style={{paddingTop:'7px'}}>Created with</p>
                 <img
                     src={image}
+                    style={{paddingBottom:'7px'}}
                     height={'30px'}
                     width={'250px'}
                 />
