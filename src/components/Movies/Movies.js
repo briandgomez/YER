9d012bdd76ae6e478b097db5a20423e7e4e07644
briandgomez <briandgomez98@gmail.com>
2022-03-16 23:39:04 -0400

    created cards using map function

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,6 +2,8 @@ import React from "react";
 import pic from '../../TMDB Color Scheme.png';
 
 const Movies = () => {
+    const moviesList = ['Brad', 'Angelina', 'Kyle!?'];
+
     return (
         <div className="movies">
             <h1>Movies</h1>
@@ -10,88 +12,20 @@ const Movies = () => {
                 <input type={"text"} placeholder="Search..." name="Search input"></input>
                 <button id="search-btn" type="Submit"><i class="fa fa-search">Go</i></button>
             </form>
-
-            <div style={{ display: "flex", flexWrap: 'wrap', justifyContent: "center" }}>
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Lorem</p>
+            <div className = "card-layout">
+                {moviesList.map(item => (
+                    <div className="card-div">
+                        <div className="card">
+                            <img src={pic} />
+                            <p key={item}
+                            >{item}</p>
+                            <div className="container">
+                                <h4><b>John Doe</b></h4>
+                                <p>Architect & Engineer</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
-
-
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img src={pic} />
-                    <div className="container">
-                        <h4><b>John Doe</b></h4>
-                        <p>Architect & Engineer</p>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     )
