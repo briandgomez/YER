7c0ba7145710ddaa52db6a3fad8cea0fb3216e5a
briandgomez <briandgomez98@gmail.com>
2022-03-19 22:16:11 -0400

    display tv posters

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 const API_KEY = 'api_key=' + process.env.REACT_APP_API_KEY;
 const IMG_SIZE = 'https://image.tmdb.org/t/p/original';
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const API_URL = BASE_URL + '/movie/popular?' + API_KEY + '&language=en-US&page=2';
 
 const Movies = () => {
     // Movies - current movie object from 'results'
@@ -13,13 +13,14 @@ const Movies = () => {
     //Arrow function indicates that everything inside {} will execute EVERTYIME the app renders
     useEffect(() => {
         fetch(API_URL)
-        .then((res) => res.json())
-        .then((data) => {
-            setMovies(data.results)
-        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data.results)
+                setMovies(data.results)
+            })
         //Empty array will call useEffect and fetch API only 1 time(when initially rendered)
         //Adding parameters inside the array will cause the useEffect function to run whenever there is a change to the parameter
-    },[])
+    }, [])
 
     return (
         <div className="movies">
@@ -34,10 +35,10 @@ const Movies = () => {
                     <div className="card-div">
                         <div className="card">
                             <img src={IMG_SIZE + item.poster_path} />
-                            <p>{item.overview}</p>
                             <div className="container">
                                 <p>{item.title}</p>
                             </div>
+                            <p>{item.overview}</p>
                         </div>
                     </div>
                 ))}
--- src/components/TV-Shows/TV.js
+++ src/components/TV-Shows/TV.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const API_KEY = 'api_key=' + process.env.REACT_APP_API_KEY;
+const IMG_SIZE = 'https://image.tmdb.org/t/p/original';
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = BASE_URL + '/tv/top_rated?' + API_KEY + '&language=en-US&page=2';
 
 const TV = () => {
+    const [tv, setTv] = useState([])
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data.results)
+                setTv(data.results)
+            })
+    }, [])
+
     return (
-        <div className="tv">
+        <div className="movies">
             <h1>TV-Shows</h1>
+
+            <form>
+                <input type={"text"} placeholder="Search..." name="Search input"></input>
+                <button id="search-btn" type="Submit"><i class="fa fa-search">Go</i></button>
+            </form>
+            <div className="card-layout">
+                {tv.map(item => (
+                    <div className="card-div">
+                        <div className="card">
+                            <img src={IMG_SIZE + item.poster_path} />
+                            <div className="container">
+                                <p>{item.name}</p>
+                            </div>
+                            <p>{item.overview}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
