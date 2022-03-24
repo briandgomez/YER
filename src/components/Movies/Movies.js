1062246e742fbe910a4643876894ae7738958dfa
briandgomez <briandgomez98@gmail.com>
2022-03-23 23:00:37 -0400

    added pagination

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from "react";
 
-const API_KEY = 'api_key=' + process.env.REACT_APP_API_KEY;
-const IMG_SIZE = 'https://image.tmdb.org/t/p/original';
-const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + '/movie/popular?' + API_KEY + '&language=en-US&page=2';
-
 const Movies = () => {
     // Movies - current movie object from 'results'
     // setMovies - changes the current movie object
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([]);
+    //Defaults 'currentPage' to 1
+    const [currentPage, setPage] = useState(1);
+
+    //Increment and decrement page by 1
+    const incrementByOne = () => {
+        setPage(currentPage + 1);
+    }
+    const decrementByOne = () => {
+        setPage(currentPage - 1);
+    }
+
+    const API_KEY = 'api_key=' + process.env.REACT_APP_API_KEY;
+    const IMG_SIZE = 'https://image.tmdb.org/t/p/original';
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const API_URL = BASE_URL + '/movie/popular?' + API_KEY + `&language=en-US&page=${currentPage}`;
 
     //Arrow function indicates that everything inside {} will execute EVERTYIME the app renders
     useEffect(() => {
         fetch(API_URL)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data.results)
+                //console.log(data.results)
                 setMovies(data.results)
             })
         //Empty array will call useEffect and fetch API only 1 time(when initially rendered)
-        //Adding parameters inside the array will cause the useEffect function to run whenever there is a change to the parameter
-    }, [])
+        //Adding 'curretPage' inside the array causes the useEffect function to run whenever there is a change to the parameter
+    }, [currentPage])
 
     return (
         <div className="movies">
@@ -30,6 +40,7 @@ const Movies = () => {
                 <input type={"text"} placeholder="Search..." name="Search input"></input>
                 <button id="search-btn" type="Submit"><i class="fa fa-search">Go</i></button>
             </form>
+
             <div className="card-layout">
                 {movies.map(item => (
                     <div className="card-div">
@@ -42,6 +53,16 @@ const Movies = () => {
                         </div>
                     </div>
                 ))}
+            </div>
+
+            <div className='directional'>
+                <button onClick={decrementByOne}>
+                    <i class="arrow left"></i>
+                </button>
+                <p>{currentPage}</p>
+                <button onClick={incrementByOne}>
+                    <i class="arrow right"></i>
+                </button>
             </div>
         </div>
     )
--- src/components/TV-Shows/TV.js
+++ src/components/TV-Shows/TV.js
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from "react";
 
-const API_KEY = 'api_key=' + process.env.REACT_APP_API_KEY;
-const IMG_SIZE = 'https://image.tmdb.org/t/p/original';
-const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + '/tv/top_rated?' + API_KEY + '&language=en-US&page=2';
-
 const TV = () => {
-    const [tv, setTv] = useState([])
+    const [tv, setTv] = useState([]);
+    const [currentPage, setPage] = useState(1);
+
+    const incrementByOne = () => {
+        setPage(currentPage + 1);
+    }
+    const decrementByOne = () => {
+        setPage(currentPage - 1);
+    }
+
+    const API_KEY = 'api_key=' + process.env.REACT_APP_API_KEY;
+    const IMG_SIZE = 'https://image.tmdb.org/t/p/original';
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const API_URL = BASE_URL + '/tv/top_rated?' + API_KEY + `&language=en-US&page=${currentPage}`;
 
     useEffect(() => {
         fetch(API_URL)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data.results)
                 setTv(data.results)
             })
-    }, [])
+    }, [currentPage])
 
     return (
         <div className="movies">
@@ -25,6 +32,7 @@ const TV = () => {
                 <input type={"text"} placeholder="Search..." name="Search input"></input>
                 <button id="search-btn" type="Submit"><i class="fa fa-search">Go</i></button>
             </form>
+
             <div className="card-layout">
                 {tv.map(item => (
                     <div className="card-div">
@@ -37,6 +45,16 @@ const TV = () => {
                         </div>
                     </div>
                 ))}
+            </div>
+
+            <div className='directional'>
+                <button onClick={decrementByOne}>
+                    <i class="arrow left"></i>
+                </button>
+                <p>{currentPage}</p>
+                <button onClick={incrementByOne}>
+                    <i class="arrow right"></i>
+                </button>
             </div>
         </div>
     )
